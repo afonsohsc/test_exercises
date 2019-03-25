6b41be36efaa2bd61aaabc429b174f48e3d98d75
Afonso Carvalho <afonsohenriquesc@hotmail.com>
2019-03-25 18:51:51 -0300

    final code

--- ManipulationLogs/Controllers/HomeController.cs
+++ ManipulationLogs/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using ManipulationLogs.Models;
 
 namespace ManipulationLogs.Controllers
 {
@@ -17,70 +18,49 @@ namespace ManipulationLogs.Controllers
         public ActionResult Index()
         {
             List<string> logsMINHACDN = new List<string>();
-            List<string> logsAgora = new List<string>();
+            List<LogsAgora> listLogsAgora = new List<LogsAgora>();
 
-            const string providerHttp = "MINHA CDN";
-            List<string> methodStatus = new List<string>();
-            List<string> codeUri = new List<string>();
-            List<string> pathTime = new List<string>();
-            List<string> takenResponse = new List<string>();
-            List<string> sizeCache = new List<string>();
-            List<string> status = new List<string>();
+			const string providerHttp = "\"MINHA CDN\"";
 
-            StreamReader reader = new StreamReader(WebRequest.Create(URL).GetResponse().GetResponseStream());
+			StreamReader reader = new StreamReader(WebRequest.Create(URL).GetResponse().GetResponseStream());
 
             while (!reader.EndOfStream)
             {
+				LogsAgora logAgora = new LogsAgora();
+
                 string line = reader.ReadLine();
                 logsMINHACDN.Add(line);
                 string[] logs = line.Split('|');
 
+				logAgora.ProviderHttp = providerHttp;
 
-                sizeCache.Add(logs[0]);
+				logAgora.SizeCache = logs[0];
 
-                codeUri.Add(logs[1]);
+				logAgora.CodeUri = logs[1];
 
-                status.Add(logs[2].Replace("INVALIDATE", "REFRESH_HIT"));
+				logAgora.Status = logs[2].Replace("INVALIDATE", "REFRESH_HIT");
 
-
-                string log = logs[3].Replace("\"", "");
+				string log = logs[3].Replace("\"", "");
 
                 string[] log4Split = log.Split(' ');
 
-                methodStatus.Add(log4Split[0]);
+				logAgora.MethodStatus = log4Split[0];
 
-                pathTime.Add(log4Split[1]);
-
+				logAgora.PathTime = log4Split[1];
 
                 string[] log5Split = logs[4].Split('.');
 
-                takenResponse.Add(log5Split[0]);
+				logAgora.TakenResponse = log5Split[0];
+
+				listLogsAgora.Add(logAgora);
 
             }
 
-            for (int i = 0; i < logsMINHACDN.Count; i++)
-            {
-                logsAgora.Add('"' + providerHttp + '"' + " " + methodStatus[i] + " " + codeUri[i] + " " + pathTime[i] + " " + takenResponse[i] + " " + sizeCache[i] + " " + status[i]);
-            }
+			ViewBag.Version = "#Version: 1.0";
+			ViewBag.Data = "#Date: " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+			ViewBag.Fields = "#Fields: provider http - method status - code uri - path time - taken response - size cache - status";
 
-            List<string> data = new List<string>() {
-            "#Version: 1.0",
-            "#Date: " + DateTime.Now.ToLocalTime(),
-            "#Fields: provider http - method status - code uri - path time - taken response - size cache - status"};
-
-            //data.Add("#Version: 1.0");
-            //data.Add("#Date: " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"));
-            //data.Add("#Fields: provider http - method status - code uri - path time - taken response - size cache - status");
-
-            ViewBag.Version = data;
-
-            //ViewBag.Version = "#Version: 1.0";
-            //ViewBag.Data = "#Date: " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
-            //ViewBag.Fields = "#Fields: provider http - method status - code uri - path time - taken response - size cache - status";
-
-            ViewBag.ListaLogsAgora = logsAgora;
-
-            return View();
+			return View(listLogsAgora);
         }
     }
 }
---
+++ ManipulationLogs/Models/LogsAgora.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ManipulationLogs.Models
+{
+	public class LogsAgora
+	{
+		public string ProviderHttp { get; set; }
+		public string MethodStatus { get; set; }
+		public string CodeUri { get; set; }
+		public string PathTime { get; set; }
+		public string TakenResponse { get; set; }
+		public string SizeCache { get; set; }
+		public string Status { get; set; }
+	}
+}
